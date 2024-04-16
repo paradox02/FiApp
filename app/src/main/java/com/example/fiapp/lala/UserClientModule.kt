893b26cde187de3bfa8713f52a54b3fd3e43e4ec
Jozef Hoschek <xhoschek@fi.muni.cz>
2024-04-16 20:57:14 +0200

    feat: partial implementation

---
+++ app/src/main/java/com/example/fiapp/lala/UserClientModule.kt
@@ -0,0 +1,15 @@
+package com.example.fiapp.lala
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class UserClientModule {
+    @Provides
+    fun provideUserClient(): UserClient {
+        return UserClient()
+    }
+}
